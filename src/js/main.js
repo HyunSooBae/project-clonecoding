90641f56322bc44fe683dc352e2aab407548caac
hyunsooBae <hyunsooBae.dev@gmail.com>
2022-10-07 21:54:22 +0900

    feat: Add css-style and swiper-plugin
    - add csc-style
    - add swiper-plugin

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,35 @@ async function test() {
   console.log(await promise)
 }
 test()
+
+// main-visual swiper-slider
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    // type: 'fraction',
+    // clickable: false,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // autoplay: {
+  //   delay: 3000,
+  // },
+
+});
+
+// 재생 일시정지 버튼
+// $(".swiper-button-pause").click(function(){
+//   homeSwiper.autoplay.stop();
+// });
+
+// $(".swiper-button-play").click(function(){
+//   homeSwiper.autoplay.start();
+// });
