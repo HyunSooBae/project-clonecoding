b0dfaac1dd704f6663c294232e88ab3dbaaf9ce4
hyunsooBae <hyunsoobae.dev@gmail.com>
2022-10-15 23:39:13 +0900

    feat: Add masonry layout
    - masonry layout을 추가함

--- src/js/main.js
+++ src/js/main.js
@@ -39,13 +39,13 @@ const swiper = new Swiper('.swiper', {
 // });
 
 // 메인 뉴스 영역 mosonry 스타일 적용
-// window.onload = () => {
-//   document.querySelectorAll('.ke-list__item').forEach((item) => {
-//     item.style.gridRowEnd = `span ${item.clientHeight}`;
-//   });
-//   const wrap = document.querySelector('.main-news__list');
-//   wrap.style.display = 'grid';
-//   wrap.style.gridTemplateColumns = 'repeat(auto-fill, 562rem)';
-//   wrap.style.gridAutoRows = 'auto';
-//   wrap.style.gridColumnGap = '120rem';
-// }
+window.onload = () => {
+  document.querySelectorAll('.ke-list__item').forEach((item) => {
+    item.style.gridRowEnd = `span ${item.clientHeight}`;
+  });
+  const wrap = document.querySelector('.main-news__list');
+  wrap.style.display = 'grid';
+  wrap.style.gridTemplateColumns = 'repeat(auto-fill, 562rem)';
+  wrap.style.gridAutoRows = 'auto';
+  wrap.style.gridColumnGap = '120rem';
+}
